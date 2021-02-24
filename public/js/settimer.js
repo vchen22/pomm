8e87417c150bc0b0cbf27c781f2920b93015d583
Vicki Chen <vchen@ucsd.edu>
2021-02-23 19:15:27 -0800

    basic features of settimer page

---
+++ public/js/settimer.js
@@ -0,0 +1,16 @@
+function setShortSession() {
+    var x = document.getElementById("work-time");
+    if (x.innerHTML === "50:00") {
+      x.innerHTML = "25:00";
+    } else {
+      x.innerHTML = "25:00";
+    }
+}
+
+function setLongSession() {
+  var x = document.getElementById("work-time");
+  if (x.innerHTML === "25:00") {
+    x.innerHTML = "50:00";
+  } else {
+    x.innerHTML = "Hello";
+}
