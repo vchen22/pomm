1a30cf104edda40423d31c8a19ef73a98cca7982
vivntng <vivntng@users.noreply.github.com>
2021-02-25 03:20:50 -0800

    strike thru completed + basic css purge

--- public/js/worksession-functions.js
+++ public/js/worksession-functions.js
@@ -1,4 +1,8 @@
-{   var start = document.getElementById('start');
+{   
+    var strike1 = document.getElementById('task1');
+    var strike2 = document.getElementById('task2');
+    var strike3 = document.getElementById('task3');
+    var start = document.getElementById('start');
     var pause = document.getElementById('pause');
     var time = document.getElementById('stopwatch'),
     seconds = 0, minutes = 0, hours = 0,
@@ -24,18 +28,43 @@ function timer() {
 }
 
 function toggle() {
-    console.log(start.style.display);
+    // console.log(start.style.display);
     if ( start.style.display === "block" ) {
-        console.log('start button pressed, pause should display');
+        // console.log('start button pressed, pause should display');
         start.style.display = "none";
         pause.style.display = "block";
     } else { // switch back
-        console.log('pause button pressed, start should display');
+        // console.log('pause button pressed, start should display');
         start.style.display = "block";
         pause.style.display = "none";
         clearTimeout(t);
     }
 }
+
+function lineThru1(){
+    if(strike1.style.textDecoration == "line-through"){
+        strike1.style.textDecoration = "none";
+    } else {
+    strike1.style.textDecoration = "line-through";
+    }
+}
+
+function lineThru2(){
+    if(strike2.style.textDecoration == "line-through"){
+        strike2.style.textDecoration = "none";
+    } else {
+    strike2.style.textDecoration = "line-through";
+    }
+}
+
+function lineThru3(){
+    if(strike3.style.textDecoration == "line-through"){
+        strike3.style.textDecoration = "none";
+    } else {
+    strike3.style.textDecoration = "line-through";
+    }
+}
+
 
 function init(){
     /* Start button */
@@ -44,6 +73,9 @@ function init(){
     start.addEventListener('click', timer);
     start.addEventListener('click', toggle);
     pause.addEventListener('click', toggle);
+    strike1.addEventListener('click', lineThru1);
+    strike2.addEventListener('click', lineThru2);
+    strike3.addEventListener('click', lineThru3);
 }
 
 // wait for DOM to load first
