3f77fcb614c340397583e5ad18a16dc24c40f52d
Vicki Chen <vchen@ucsd.edu>
2021-02-25 02:53:24 -0800

    populated profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,12 @@
  * GET profile page.
  */ 
 // create another view/file
-exports.viewProfile = function(req, res){
-    res.render('profile');
+// exports.viewProfile = function(req, res){
+//     res.render('profile');
+// };
+
+var data = require('../userprofile.json');
+
+exports.viewProfile = function(req, res) {
+    res.render('profile', data);
 };
