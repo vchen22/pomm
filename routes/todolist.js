0e12b3bca636c6ccb3d19e3801f3c2417755f2ee
Jarrett Cornelison <jcornel00@gmail.com>
2021-03-04 05:58:38 -0800

    Implemented task saving for todo lists and work session with Firebase

--- routes/todolist.js
+++ routes/todolist.js
@@ -1,12 +1,12 @@
 /*
  * GET to do list page.
- */ 
+ */
 // create another view/file
-var taskData = require("../taskname.json");
+var taskData = require('../taskname.json');
 
-exports.viewToDoList = function(req, res){
-    console.log("in todolist.js")
-    console.log(taskData);
-    // res.render('todolist');
-    res.json(taskData);
+exports.viewToDoList = function (req, res) {
+  console.log('in todolist.js');
+  console.log(taskData);
+  res.render('todolist');
+  //res.json(taskData);
 };
--- routes/worksession.js
+++ routes/worksession.js
@@ -1,11 +1,10 @@
-
 /*
  * GET work session.
- */ 
+ */
 
 var data = require('../taskname.json');
 
-exports.viewWorkSession = function(req, res){
-    console.log(data);
-    res.render('worksession', data);
-};a
+exports.viewWorkSession = function (req, res) {
+  console.log(data);
+  res.render('worksession', data);
+};
