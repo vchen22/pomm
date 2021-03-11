9043048c80772c5305d4e49e0df9d8b3ebb76730
Jarrett Cornelison <jcornel00@gmail.com>
2021-03-10 17:53:47 -0800

    Added percentage to progress bar and case for when user has no pomms

--- public/js/profileFill.js
+++ public/js/profileFill.js
@@ -27,13 +27,12 @@ auth.onAuthStateChanged((user) => {
         // document.getElementById('sessions').innerHTML = sessionsHTML;
         // document.getElementById('pomms').innerHTML = pommsHTML;
         // document.getElementById('minutes').innerHTML = minutesHTML;
-        $("#name").html(nameHTML);
-        $("#sessions").html(sessionsHTML);
-        $("#pomms").html(pommsHTML);
-        $("#minutes").html(minutesHTML);
+        $('#name').html(nameHTML);
+        $('#sessions').html(sessionsHTML);
+        $('#pomms').html(pommsHTML);
+        $('#minutes').html(minutesHTML);
 
         let pommProgress = pomms * 20 - doc.data().minutes;
-        //console.log(pommProgress);
 
         // if (pommProgress < 0) {
         //   // progress-text
@@ -50,23 +49,54 @@ auth.onAuthStateChanged((user) => {
         // } else {
         //   document.getElementById('pomm-progress').style.width = '0%';
         // }
-
-        if (pommProgress < 0) {
-          $('#pomm-progress').css("width", "100%");
+        if (pomms == 0) {
+          $('#pomm-progress').css('width', '0%');
+          var maxHTML = `<h2 id="progress-text">Start Studying To Get Some Pomms!</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 0) {
+          $('#pomm-progress').css('width', '100%');
           var maxHTML = `<h2 id="progress-text">Max Pomms Achieved!</h2>`;
           $('#progress-text').html(maxHTML);
-        }
-        else if (pommProgress < 5) {
-          $('#pomm-progress').css("width", "75%");
-        }
-        else if (pommProgress < 10) {
-          $('#pomm-progress').css("width", "50%");
-        }
-        else if (pommProgress < 15) {
-          $('#pomm-progress').css("width", "25%");
-        }
-        else {
-          $('#pomm-progress').css("width", "0%");
+        } else if (pommProgress < 2) {
+          $('#pomm-progress').css('width', '95%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 95%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 4) {
+          $('#pomm-progress').css('width', '85%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 85%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 6) {
+          $('#pomm-progress').css('width', '75%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 75%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 8) {
+          $('#pomm-progress').css('width', '65%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 65%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 10) {
+          $('#pomm-progress').css('width', '55%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 55%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 12) {
+          $('#pomm-progress').css('width', '45%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 45%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 14) {
+          $('#pomm-progress').css('width', '35%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 35%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 16) {
+          $('#pomm-progress').css('width', '25%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 25%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else if (pommProgress < 18) {
+          $('#pomm-progress').css('width', '15%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 15%</h2>`;
+          $('#progress-text').html(maxHTML);
+        } else {
+          $('#pomm-progress').css('width', '0%');
+          var maxHTML = `<h2 id="progress-text">Progress To Next Pomm: 0%</h2>`;
+          $('#progress-text').html(maxHTML);
         }
 
         if (profileBool) {
@@ -76,7 +106,7 @@ auth.onAuthStateChanged((user) => {
             .getDownloadURL()
             .then((imgUrl) => {
               // document.getElementById('pfp').src = imgUrl;
-              $('#pfp').attr("src", imgUrl);
+              $('#pfp').attr('src', imgUrl);
             });
         }
       });
