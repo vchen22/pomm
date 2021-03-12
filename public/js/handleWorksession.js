e319e63b561011ce7af90fe01a137944c4255089
Jarrett Cornelison <jcornel00@gmail.com>
2021-03-11 23:59:21 -0800

    Added confirmation for removing tasks in worksession

--- public/js/handleWorksession.js
+++ public/js/handleWorksession.js
@@ -1,34 +1,36 @@
 var totalTasks = 0;
 
 function handleRemove(num) {
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      db.collection('users')
-        .doc(user.uid)
-        .get()
-        .then((doc) => {
-          let dbTasks = doc.data().tasks;
+  if (confirm('Are you sure you are finished with this task?')) {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        db.collection('users')
+          .doc(user.uid)
+          .get()
+          .then((doc) => {
+            let dbTasks = doc.data().tasks;
 
-          //console.log(dbTasks);
+            //console.log(dbTasks);
 
-          // remove from db
-          //const index = dbTasks.indexOf(0);
-          dbTasks.splice(num, 1);
+            // remove from db
+            //const index = dbTasks.indexOf(0);
+            dbTasks.splice(num, 1);
 
-          //console.log(dbTasks);
+            //console.log(dbTasks);
 
-          totalTasks++;
+            totalTasks++;
 
-          console.log(totalTasks);
+            console.log(totalTasks);
 
-          db.collection('users').doc(user.uid).update({
-            tasks: dbTasks,
+            db.collection('users').doc(user.uid).update({
+              tasks: dbTasks,
+            });
           });
-        });
-    } else {
-      console.log('user not logged in');
-    }
-  });
+      } else {
+        console.log('user not logged in');
+      }
+    });
+  }
 }
 
 auth.onAuthStateChanged((user) => {
